10ff96e906d81938662d748dd082ecdcc5d25eaa
ayushmodi001 <2303031057006@paruluniversity.ac.in>
2025-05-26 07:49:29 +0530

    day 12

---
+++ arrays_sort.java
@@ -0,0 +1,5 @@
+public class arrays_sort {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ sort_array_ascen.java
@@ -0,0 +1,5 @@
+public class sort_array_ascen {
+    public static void main(String[] args) {
+        
+    }
+}
